ce44e6c8212d488c4c636c9e97efa4b6262e695d
cmetgher <cmetgher@gmu.edu>
2014-05-22 22:45:54 -0400

    Initial Commit

---
+++ jucarii/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jucarii
+//
+//  Created by Cristina Metgher on 5/22/14.
+//  Copyright (c) 2014 cmetgher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
